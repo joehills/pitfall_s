c7c58fe3f99f7b4e3dcb88c7402a5ce8ceac2778
Joe Hills <joe@teamsnowday.com>
2015-04-02 23:28:48 -0400

    moved containers where they belong

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-
+<div class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -52,5 +52,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+</div><!-- /.container -->
 
 <?php get_footer(); ?>
